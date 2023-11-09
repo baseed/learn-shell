f56d7c9c0a305f01a7fb218385fae38688a88bc1
baseed <baseed08@gmail.com>
2023-11-09 12:09:13 +0530

    expense script

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,11 +1,11 @@
 fruit=$1
 
-if [ $fruit == "apple" ]; then
+if [ $fruit  == "apple" ]; then
   echo apple price is 1$
   fi
 
 
-if [ $fruit == "orange" ]; then
+if [ $fruit  == "orange" ]; then
   echo orange price is 2$
   fi
 
