c0089ba8183ce74256dd4e15d0556243981cf0c7
baseed <baseed08@gmail.com>
2023-11-12 11:59:49 +0530

    expense script

--- 07-cals.sh
+++ 07-cals.sh
@@ -5,3 +5,15 @@
  echo free_percentage $free_percentage
   used_percentage=$(echo "100 - $free / $total * 100" | bc -l | awk -F . '{print $1}')
   echo used_percentage $used_percentage
+
+  # used_percentage=0-50  good
+  # 50-70 orange
+  # >70 Red
+
+  if [ used_percentage -lt 50 ]; then
+     echo Mem start is GREEN
+  elif [ used_percentage -lt 70 ]; then
+     echo Mem start is orange
+  else
+     echo Mem is Red
+  fi
