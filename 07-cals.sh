44a1bf0ab73cc0201f71c95365594129a05ed8f0
baseed <baseed08@gmail.com>
2023-11-12 11:42:53 +0530

    expense script

--- 07-cals.sh
+++ 07-cals.sh
@@ -4,4 +4,4 @@
  free_percentage=$(echo "$free / $total * 100" | bc -l | awk -F . '{print $1}')
  echo free_percentage $free_percentage
   used_percentage=$(echo "100 - $free / $total * 100" | bc -l | awk -F . '{print $1}')
-  echo used_percentage $ufree_percentage
+  echo used_percentage $used_percentage
